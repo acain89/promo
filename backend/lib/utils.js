e4c07d8d8bab422050b6f751b867b693455d0562
Andrew Cain <acain89@gmail.com>
2026-02-08 17:59:27 -0600

    Export mmddyyyyFromCutoffMs in utils

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -60,3 +60,11 @@ export function parseCookies(req) {
   });
   return out;
 }
+
+export function mmddyyyyFromCutoffMs(ms) {
+  const d = new Date(Number(ms || 0));
+  const mm = String(d.getMonth() + 1).padStart(2, "0");
+  const dd = String(d.getDate()).padStart(2, "0");
+  const yyyy = String(d.getFullYear());
+  return `${mm}/${dd}/${yyyy}`;
+}
